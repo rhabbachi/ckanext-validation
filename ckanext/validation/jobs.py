4d2fa0f25f51145cde24dda389c6b2c71de912e3
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-03-18 11:08:00 +0000

    ADX-225 Allow validation of json files as well as geojson files

--- ckanext/validation/jobs.py
+++ ckanext/validation/jobs.py
@@ -178,6 +178,7 @@ def run_validation_job(resource):
 
 
 def _load_dataframe(data, extension):
+
     # Read in table
     if extension == "csv":
         df = _read_csv_file(data, extension)
@@ -185,12 +186,12 @@ def _load_dataframe(data, extension):
         df = _read_excel_file(data, extension)
     elif extension in ["shp"]:
         df = _read_shape_file(data)
-    elif extension in ['geojson']:
-        df = _read_geojson_file(data)
+    elif extension in ['json', 'geojson']:
+        df = _read_json_file(data)
     else:
         raise t.ValidationError({
             _('Incorrect Extension'): [_('Cannot validate the file. Please check '
-                                    'the file extension is correct.')]
+                                         'the file extension is correct.')]
         })
     df.columns = df.iloc[0]
     df.index = df[df.columns[0]]
@@ -236,18 +237,18 @@ def _read_excel_file(data, extension=None):
     return df
 
 
-def _read_geojson_file(geojson_path):
+def _read_json_file(json_path):
     """
-    Reads a geojson file in as a pandas dataframe ready for validation.
+    Reads a json or geojson file in as a pandas dataframe ready for validation.
     """
     # Load as plain JSON
     try:
-        with open(geojson_path, 'r') as read_file:
+        with open(json_path, 'r') as read_file:
             geojson = json.load(read_file)
     except Exception as e:
         log.exception(e)
         raise t.ValidationError({
-            'GeoJSON': [_(u'Unable to import json: ') + str(e)]
+            'JSON': [_(u'Unable to import JSON: ') + str(e)]
         })
 
     # Structure the data
@@ -260,8 +261,9 @@ def _read_geojson_file(geojson_path):
         df_dict = map(create_row, geojson['features'])
 
     except Exception as e:
+        log.exception(e)
         raise t.ValidationError({
-            'GeoJSON': [_(u'Unable to import geoJSON: ') + str(e)]
+            'JSON': [_(u'Unable to import JSON: ') + str(e)]
         })
 
     # Create the dataframe and insert the headers as the first row
